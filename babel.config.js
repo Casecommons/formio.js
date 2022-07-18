548f29eeac36bb4f1b5b1f1fb8d3477ee091fa70
Bisoye Atolagbe <atolagbe.bisoye@andela.com>
2022-07-18 13:48:40 +0100

    update build file

--- babel.config.js
+++ babel.config.js
@@ -9,8 +9,8 @@ module.exports = {
     ],
   ],
   plugins: [
-    '@babel/plugin-proposal-class-properties',
+    ['@babel/plugin-proposal-class-properties',  { 'loose': true }],
     '@babel/plugin-proposal-export-default-from',
-    '@babel/plugin-proposal-optional-chaining'
+    '@babel/plugin-proposal-optional-chaining',
   ],
 };
