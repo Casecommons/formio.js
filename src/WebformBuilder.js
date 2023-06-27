378431abf71661f2b2ffcd4ae831dee7c1ea16d3
Bisoye Atolagbe <atolagbe.bisoye@andela.com>
2023-06-27 18:39:42 +0100

    fix component issue

--- src/WebformBuilder.js
+++ src/WebformBuilder.js
@@ -1150,16 +1150,18 @@ export default class WebformBuilder extends Component {
         const parentComponent = defaultValueComponent.parent;
         let tabIndex = -1;
         let index = -1;
-        parentComponent.tabs.some((tab, tIndex) => {
-          tab.some((comp, compIndex) => {
-            if (comp.id === defaultValueComponent.id) {
-              tabIndex = tIndex;
+        if (parentComponent && parentComponent.tabs) {
+          parentComponent.tabs.some((tab, tIndex) => {
+            tab.some((comp, compIndex) => {
+              if (comp.id === defaultValueComponent.id) {
+                tabIndex = tIndex;
               index = compIndex;
               return true;
             }
             return false;
           });
         });
+      }
 
         if (tabIndex !== -1 && index !== -1 && changed && changed.value) {
           const sibling = parentComponent.tabs[tabIndex][index + 1];
