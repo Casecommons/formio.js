17086180ed670379b41fbc503e49d44aacbee9ed
Bisoye Atolagbe <atolagbe.bisoye@andela.com>
2023-06-27 12:12:42 +0100

    update signature component

--- src/components/signature/Signature.js
+++ src/components/signature/Signature.js
@@ -145,12 +145,8 @@ export default class SignatureComponent extends Input {
     if (this.refs.padBody && (force || this.refs.padBody && this.refs.padBody.offsetWidth !== this.currentWidth)) {
       this.scale = force ? scale : this.scale;
       this.currentWidth = this.refs.padBody.offsetWidth;
-      const width = this.currentWidth * this.scale;
-      this.refs.canvas.width = width;
-      const height = this.ratio ? width / this.ratio : this.refs.padBody.offsetHeight * this.scale;
-      this.refs.canvas.height = height;
-      this.refs.canvas.style.maxWidth = `${this.currentWidth * this.scale}px`;
-      this.refs.canvas.style.maxHeight = `${this.refs.padBody.offsetHeight * this.scale}px`;
+      this.refs.canvas.width = this.currentWidth * this.scale;
+      this.refs.canvas.height = this.refs.padBody.offsetHeight * this.scale;
       const ctx = this.refs.canvas.getContext('2d');
       ctx.setTransform(1, 0, 0, 1, 0, 0);
       ctx.scale((1 / this.scale), (1 / this.scale));
